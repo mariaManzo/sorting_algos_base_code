b05f671a1c67449c4ba0966c0579a56508dbd927
Maria <maria.manzo@le-campus-numerique.fr>
2020-11-10 17:04:51 +0100

    implementation insertionsort, selectionsort et bubblesort

--- js/algos.js
+++ js/algos.js
@@ -7,12 +7,31 @@ Number.prototype.toRadians = function() {
 // Calculates the distance between Grenoble and the given city
 function distanceFromGrenoble(city)
 {
-  console.log("distanceFromGrenoble - implement me !");
+  //console.log("distanceFromGrenoble - implement me !");
   var GrenobleLat = 45.166667;
   var GrenobleLong = 5.716667;
+  var cityLat = city.latitude;
+  var cityLong = city.longitude;
 
-  return 1;
+  const R = 6371e3; // metres
+  const φ1 = GrenobleLat * Math.PI/180; // φ, λ in radians
+  const φ2 = cityLat * Math.PI/180;
+  const Δφ = (cityLat-GrenobleLat) * Math.PI/180;
+  const Δλ = (cityLong-GrenobleLong) * Math.PI/180;
+
+  const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+      Math.cos(φ1) * Math.cos(φ2) *
+      Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  const d = R * c; // in metres
+  const distInKm = d /1000;
+
+  return distInKm;
+
 }
+
+
 
 // Swap 2 values in array csvData
 // i is the index of the first city
@@ -20,7 +39,9 @@ function distanceFromGrenoble(city)
 function swap(i,j)
 {
   displayBuffer.push(['swap', i, j]); // Do not delete this line (for display)
-  console.log("swap - implement me !");
+  let tmp = csvData[i];
+  csvData[i] = csvData[j];
+  csvData[j] = tmp;
 
 }
 
@@ -30,23 +51,47 @@ function swap(i,j)
 function isLess(i, j)
 {
   displayBuffer.push(['compare', i, j]); // Do not delete this line (for display)
-  console.log("isLess - implement me !");
+  return distanceFromGrenoble(csvData[i]) < distanceFromGrenoble(csvData[j]);
 }
 
 
-function insertsort()
-{
-  console.log("insertsort - implement me !");
+function insertsort(){
+
+  for (let i = 1; i < csvData.length; i++){
+    for (let j = i; j > 0 && isLess(j, j-1); j--){
+      swap(j,j-1);
+    }
+  }
 }
 
 function selectionsort()
 {
-  console.log("selectionsort - implement me !");
+  for(var i = 0; i < csvData.length-1; i++){
+    //stocker l'index de l'élément minimum
+    var min = i;
+    for(var j = i+1; j < csvData.length; j++){
+      if(isLess(j,min)){
+        // mettre à jour l'index de l'élément minimum
+        min = j;
+      }
+    }
+    swap(i, min);
+  }
+
 }
 
 function bubblesort()
 {
-  console.log("bubblesort - implement me !");
+  let isSwapped
+  do {
+    isSwapped = false
+    for (let i = 0; i < csvData.length-1; i++) {
+      if (!isLess(i, i+1)) {
+        swap(i, i+1)
+        isSwapped = true
+      }
+    }
+  } while (isSwapped)
 }
 
 function shellsort()
@@ -64,10 +109,14 @@ function heapsort()
   console.log("heapsort - implement me !");
 }
 
-function quicksort()
-{
-  console.log("quicksort - implement me !");
-}
+
+  function quicksort()
+  {
+
+  }
+
+
+
 function quick3sort()
 {
   console.log("quick3sort - implement me !");
